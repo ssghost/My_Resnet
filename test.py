0b1d86f8b9c53f6513273efeadee1d521c66dacb
Shuang Song <6882971+ssghost@users.noreply.github.com>
2019-07-30 13:41:34 +0700

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import resnet
 
 def main():
     try:
-        opts, args = getopt.getopt(sys.argv[1:], 'i:o:r:c:l', ['images=','outdir=','resize=','crop=','loadpath='])
+        opts, args = getopt.getopt(sys.argv[1:], 'i:o:r:c:m:', ['images=','outdir=','resize=','crop=','loadmodel='])
     except getopt.GetoptError as err:
         print(err) 
         sys.exit()
@@ -21,10 +21,11 @@ def main():
             resize = [a,a]
         elif o in ('-c', '--crop') and type(a)==int:
             crop = [a,a]
-        elif o in ('-l', '--loadpath') and type(a)==str:
+        elif o in ('-m', '--loadmodel') and type(a)==str:
             loadpath = a
         else:
             assert False, 'unhandled option'
+            
     if loadpath == None:
         resnet().input(ipath=ipath,resize=resize,crop=crop)
         resnet().test(opath)
