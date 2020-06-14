d7571d36a5ab7fabbec58bc4ba1e90046f383f99
Shuang Song <6882971+ssghost@users.noreply.github.com>
2020-06-14 18:06:26 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 import getopt, sys
-from resnet import *
+from resnet import Resnet
 
 def main():
     try:
-        opts, args = getopt.getopt(sys.argv[1:], 'i:o:r:c:m:', ['images=','outdir=','resize=','crop=','loadmodel='])
+        opts, args = getopt.getopt(sys.argv[1:], 'i:o:r:c:m:', ['inpath=','outpath=','resize=','crop=','modelpath='])
     except getopt.GetoptError as err:
         print(err) 
         sys.exit()
@@ -13,24 +13,25 @@ def main():
     loadpath = None
 
     for o, a in opts:
-        if o in ('-i', '--images') and type(a)==str:
+        if o in ('-i', '--inpath') and type(a)==str:
             ipath = a
-        elif o in ('-o', '--outdir') and type(a)==str:
+        elif o in ('-o', '--outpath') and type(a)==str:
             opath = a 
         elif o in ('-r', '--resize') and type(a)==int:
             resize = [a,a]
         elif o in ('-c', '--crop') and type(a)==int:
             crop = [a,a]
-        elif o in ('-m', '--loadmodel') and type(a)==str:
-            loadpath = a
+        elif o in ('-m', '--modelpath') and type(a)==str:
+            modelpath = a
         else:
             assert False, 'unhandled option'    
     
+    res = Resnet()
     if loadpath != None:
-        resnet().load_model(loadpath)
+        res.load_model(modelpath)
         
-    resnet().input(ipath=ipath,lpath=None,resize=resize,crop=crop)
-    resnet().test(opath)
+    res.input(ipath=ipath,lpath=None,resize=resize,crop=crop)
+    res.test(opath)
     
 if __name__ == "__main__":
     main()
